7e3561492debf57c041053db926d06119943ff53
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-07-08 04:23:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var KatzDeliLine = [];
+
+function takeANumber(KatzDeliLine, name) {
+  KatzDeliLine.push(`${name}`);
+  return (`Welcome, ${name}. you are number ${KatzDeliLine.length} in line.`);
+}
+
+function nowServing(KatzDeliLine) {
+  return
+}
